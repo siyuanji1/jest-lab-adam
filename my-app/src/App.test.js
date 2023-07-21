e82797c78a35560c115047bbc8c6b4b8e07f26f0
siyuanji1 <siyuanji@uw.edu>
2023-07-20 19:43:03 -0700

    Lab Done

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen.debug();
+  userEvent.click(screen.getByTestId("checkbox"));
+  // if not checked
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+
+});
+test("tesing for Hello World", () => {
+  render(<App />);
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
 });
